6bbf8da407cc5a09d581d34ae9a331b0dcd4d7e0
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-01-26 16:09:04 -0600

    Add form-validation.js

---
+++ form-validation.js
@@ -0,0 +1,26 @@
+// form validation 
+
+// get the form => add Event listner to submit
+const form = document.forms[0];
+
+form.addEventListener('submit', (event) => {
+
+});
+/* ---------------------- */
+
+
+function validate(event) {
+// check if input 'name' maxlength = 30
+
+
+
+// check if email is written properly '_replyto'
+
+// text area max length < 300 char  'message'
+form.preventDefault()
+}
+
+
+
+
+
