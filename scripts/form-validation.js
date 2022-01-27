f5c55d2b0ef4c339e1966e4b343439cf755cdd52
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-01-27 07:22:11 -0600

    Add validateEmail function

---
+++ scripts/form-validation.js
@@ -0,0 +1,25 @@
+// form validation 
+function validateEmail(form) {
+  // check if email is written properly '_replyto'
+  const email = form.elements['_replyto'].value
+  
+  //  to check if email was written only using lowercase char
+  const emailRegex = /^([a-z0-9_-]+)@([a-z0-9_-]+).((([a-z]){3})|((([a-z]){3}).(([a-z]){1,3})))$/
+  
+  return emailRegex.test(email)
+}
+
+// get the form => add Event listner to submit
+const contactForm = document.forms[0];
+
+contactForm.addEventListener('submit', (event) => {
+  const checkEmail = validateEmail(contactForm)
+  event.preventDefault()
+});
+/* ---------------------- */
+
+
+
+
+
+
