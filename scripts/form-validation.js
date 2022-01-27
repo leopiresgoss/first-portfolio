dabd2a7ad7a50059c1c575fad097d56b5815e763
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-01-27 10:39:00 -0600

    Improve email validation style
    Now, this script wil add the error content to html

--- scripts/form-validation.js
+++ scripts/form-validation.js
@@ -1,32 +1,20 @@
-// form validation 
-function validateEmail(form) {
-  // check if email is written properly '_replyto'
-  const email = form.elements['_replyto'].value
-  
-  //  to check if email was written only using lowercase char
-  const emailRegex = /^([a-z0-9_-]+)@([a-z0-9_-]+).((([a-z]){3})|((([a-z]){3}).(([a-z]){1,3})))$/
-  
-  return emailRegex.test(email)
+function validateEmail() {
+  const email = document.getElementById('email').value;
+
+  // check if email was written only using lowercase char
+  const emailRegex = /^([a-z0-9_-]+)@([a-z0-9_-]+).((([a-z]){3})|((([a-z]){3}).(([a-z]){1,3})))$/;
+
+  return emailRegex.test(email);
 }
 
-// get the form => add Event listner to submit
 const contactForm = document.forms[0];
-console.log(contactForm);
 
 contactForm.addEventListener('submit', (event) => {
-  const checkEmail = validateEmail(contactForm);
-  if(!checkEmail){
-    
-    const errormsg = document.querySelector('.error-msg');
-    console.log(errormsg);
-    errormsg.style.display = 'block';
+  const checkEmail = validateEmail();
+  if (!checkEmail) {
+    const errorMsg = document.querySelector('.error-msg');
+    errorMsg.innerHTML = "Please enter your email addres in this format: 'example@example.com'";
+    errorMsg.style.display = 'block';
+    event.preventDefault();
   }
-  event.preventDefault()
 });
-/* ---------------------- */
-
-
-
-
-
-
