528be410cfe5578ed626dcfc2c3ad2d3e302e630
Ntare Guy <gntare2@gmail.com>
2022-01-28 00:38:44 +0200

    Added populate data from local storage

--- scripts/local-storage.js
+++ scripts/local-storage.js
@@ -11,6 +11,7 @@ if (formDataStored) {
 
 // add event listener to input and change local storage
 const inputs = document.querySelectorAll('form fieldset > *');
+
 inputs.forEach((input) => {
   input.addEventListener('change', () => {
     formData[input.id] = input.value;
@@ -18,3 +19,18 @@ inputs.forEach((input) => {
     localStorage.setItem('formData', formDataJSON);
   });
 });
+
+
+
+window.addEventListener('load',()=>{
+  const data = formData;
+  const username = document.querySelector('#full-name');
+  const useremail = document.querySelector('#email');
+  const usermessage = document.querySelector('#message');
+  console.log('+++++==+++++++++++',data);
+  username.value=data['full-name'];
+  useremail.value=data['email'];
+  usermessage.value=data['message'];
+
+
+})
