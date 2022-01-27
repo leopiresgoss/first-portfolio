32787b6f25dc652516e695ebaa51bf5e8d70779e
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-01-27 15:12:19 -0600

    Get form data from localstorage

--- scripts/local-storage.js
+++ scripts/local-storage.js
@@ -1,9 +1,13 @@
 // create JS object
-const formData = {};
+let formData = {};
 
 // check if there is localstorage
 // add localstorage to formData obj
 // load page with formData obj
+const formDataStored = localStorage.getItem('formData');
+if (formDataStored) {
+  formData = JSON.parse(formDataStored);
+}
 
 // add event listener to input and change local storage
 const inputs = document.querySelectorAll('form fieldset > *');
